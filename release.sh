52e03015ba65f1d95bf5bde5c9386e621cddd236
Imre Tabur <imre.tabur@eesti.ee>
2020-04-04 19:43:10 +0300

    version-1.4.6-SNAPSHOT

--- release.sh
+++ release.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
 # MIT License
-# Copyright (c) 2017-2019 Imre Tabur <imre.tabur@eesti.ee>
+# Copyright (c) 2017-2020 Imre Tabur <imre.tabur@eesti.ee>
 
+NAME=servedjs
 RELEASE=1.4.6-SNAPSHOT
 VERSION_TAG=version-${RELEASE}
 
-npm install && npm run build && npm run test
+npm install
+npm audit fix
+npm ci
+npm run build
+npm run test
+npm run unit
 git add ./dist package.json package-lock.json release.sh src/frontend/public/js/
 git commit -m "${VERSION_TAG}"
 git push
 git checkout master
 git merge develop
-git tag -a ${VERSION_TAG} -m "${VERSION_TAG}" && git push origin ${VERSION_TAG} && git push && npm publish
+git tag -a ${VERSION_TAG} -m "${VERSION_TAG}"
+git push origin ${VERSION_TAG}
+git push
+npm publish
 
 exit 0
